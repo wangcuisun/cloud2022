413688d5c7b97b6f66f5b3b8d8058c1d910e32ad
wangcuisun <wangcuisun@outlook.com>
2022-03-03 21:30:53 +0800

    cloud-consumer-order80

---
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
@@ -0,0 +1,34 @@
+package com.atguigu.springcloud.controller;
+
+import com.atguigu.springcloud.entities.CommonResult;
+import com.atguigu.springcloud.entities.Payment;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+@RestController
+@Slf4j
+public class OrderController {
+
+    public static final String PAYMENT_URL = "http://localhost:8001";
+
+    @Resource
+    private RestTemplate restTemplate;
+
+    @GetMapping("/consumer/payment/create")
+    public CommonResult create(Payment payment) {
+        String url = PAYMENT_URL + "/payment/create";
+        // 被调用者的函数里要加@RequestBody注解
+        return restTemplate.postForObject(url, payment, CommonResult.class);
+    }
+
+    @GetMapping("/consumer/payment/get/{id}")
+    public CommonResult getPaymentById(@PathVariable("id") Long id) {
+        String url = PAYMENT_URL + "/payment/get/" + id;
+        return restTemplate.getForObject(url, CommonResult.class);
+    }
+}

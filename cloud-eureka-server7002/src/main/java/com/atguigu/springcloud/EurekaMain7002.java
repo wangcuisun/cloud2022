bb9c03f3fa86e5b93dc3dfb265b4ec31e880bc18
wangcuisun <wangcuisun@outlook.com>
2022-03-04 20:36:41 +0800

    eureka服务集群搭建，并把80和8001注册进集群中

---
+++ cloud-eureka-server7002/src/main/java/com/atguigu/springcloud/EurekaMain7002.java
@@ -0,0 +1,13 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer //注册中心需要增加此配置
+@SpringBootApplication
+public class EurekaMain7002 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7002.class, args);
+    }
+}

88d155f0ba6b49c694f5546249b58086bd1792a9
wangcuisun <wangcuisun@outlook.com>
2022-03-04 21:02:01 +0800

    payment服务集群搭建

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/config/Config.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/config/Config.java
@@ -1,11 +1,13 @@
 package com.atguigu.springcloud.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class Config {
+    @LoadBalanced //开启RestTemplate的负载均衡
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
